bcd5d4788e18867ce6496d283874447bf3fe2b55
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-21 15:15:44 +1000

    update axios and js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function tellmejokes(response) {
-  let joke = response.data.generated_text;
+  let joke = response.data.answer;
   document.querySelector("#joke-display").innerHTML = joke;
 }
 
 let apiKey = "3t4aaf46b1o5b1534f3e944eb00eb10d";
-let context = "tell me a good ethical joke";
+let context = "provide a short answer and make sure to pick one";
+let prompt = "tell me a good ethical joke";
 
 document.querySelector("#joke-button").addEventListener("click", function () {
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?context=${context}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(tellmejokes);
 });
